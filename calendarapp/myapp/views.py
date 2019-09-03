d1f1e9bb65d968d3102a3876dd445b1aefa9a0c4
atiwat.github.io <atiwat.ka.61@ubu.ac.th>
2019-09-03 15:59:06 +0700

    Lab

---
+++ calendarapp/myapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+def index(req):
+  return render(req, 'myapp/index.html')
+
+def index(req):
+  return render(req, 'myapp/myapp.html')
+# Create your views here.
